b8b5bb8e1d80480d1f47040bc57f2c6aa1b689ba
Leif Denby <leif@denby.eu>
2020-07-30 14:41:42 +0100

    Add test to validate entire catalog
    Adds test which walks the entire catalog (max depth 10) and ensures the
    data can be accessed and loaded from each data-source.

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -1,8 +1,34 @@
-from intake import open_catalog
+from pathlib import Path
 
-CATALOG_URL = "https://raw.githubusercontent.com/leifdenby/eurec4a-intake/master/catalog.yml"
 
-def test_catalog_discover():
-    cat = open_catalog(CATALOG_URL)
-    cat.discover()
-    cat.radiosondes.bco.discover()
+import intake
+import pytest
+
+
+def get_master_catalog():
+    # TODO: replace with environment variable
+    p_catalog = Path(__file__).parent.parent/"catalog.yml"
+    assert p_catalog.exists()
+    return intake.open_catalog(str(p_catalog))
+
+
+@pytest.fixture
+def catalog():
+    return get_master_catalog()
+
+
+ALL_ENTRIES = list(get_master_catalog().walk(depth=10))
+@pytest.mark.parametrize("dataset_name", ALL_ENTRIES)
+def test_get_intake_source(catalog, dataset_name):
+    item = catalog[dataset_name]
+    if item.container == "catalog":
+        item.reload()
+    else:
+        item.discover()
+        plugin = item.describe()['plugin'][0]
+        if plugin in ["opendap", "zarr"]:
+            _ = item.to_dask()
+        elif plugin in ["intake_esm.esm_datastore", "parquet"]:
+            _ = item.get()
+        else:
+            raise Exception(plugin)
