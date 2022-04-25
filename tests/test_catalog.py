b9fc1a6ca934b26553f3bb4c67c56691d0cd962e
Tobias Kölling <tobias.koelling@mpimet.mpg.de>
2022-04-25 16:17:05 +0200

    run tests only for catalog files which have been modified by the change

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -4,10 +4,11 @@ from pathlib import Path
 import intake
 import pytest
 
+def root_path():
+    return Path(__file__).parent.parent
 
 def get_master_catalog():
-    # TODO: replace with environment variable
-    p_catalog = Path(__file__).parent.parent/"catalog.yml"
+    p_catalog = root_path()/"catalog.yml"
     assert p_catalog.exists()
     return intake.open_catalog(str(p_catalog))
 
@@ -16,9 +17,24 @@ def get_master_catalog():
 def catalog():
     return get_master_catalog()
 
+def all_entries(reference_branch="master"):
+    from sh import git
+    relevant_files = set(l.strip()
+        for l in git.log("--oneline", "--name-only", f"{reference_branch}..HEAD", _tty_out=False, _iter=True))
 
-ALL_ENTRIES = list(get_master_catalog().walk(depth=10))
-@pytest.mark.parametrize("dataset_name", ALL_ENTRIES)
+    root = root_path()
+    relevant_files = set(root/f for f in relevant_files if (root/f).exists())
+
+    catalog = get_master_catalog()
+
+    def marks_for(e):
+        if Path(catalog[e].catalog_object.path) in relevant_files:
+            yield pytest.mark.modified_on_branch
+
+    return [pytest.param(e, marks=list(marks_for(e)))
+            for e in get_master_catalog().walk(depth=10)]
+
+@pytest.mark.parametrize("dataset_name", all_entries())
 def test_get_intake_source(catalog, dataset_name):
     item = catalog[dataset_name]
     if item.container == "catalog":
